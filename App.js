1fe5ab8e2fa754a6f66730954053139d2fed4038
lucas-catto <lucascattosilva2018@gmail.com>
2024-08-02 22:21:20 -0300

    Estudando um Botão.

--- App.js
+++ App.js
@@ -1,35 +1,29 @@
-import { Text, SafeAreaView, StyleSheet } from 'react-native';
 
-// You can import supported modules from npm
-import { Card } from 'react-native-paper';
+import React from 'react';
 
-// or any files within the Snack
-import AssetExample from './components/AssetExample';
+import {
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+
+import {
+  Button,
+} from 'react-native-paper';
 
 export default function App() {
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
+      <Button icon="camera" mode="outilined" onPress={() => console.log('pressed')}>Press Me</Button>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    flex:            1,
+    justifyContent:  'center',
     backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    padding:         8,
   },
 });
